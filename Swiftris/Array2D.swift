f9b5a7fb2ec3b3e3c1a7ee8eddc8f51df4fb8a43
Stan Idesis <stan@mindgap.com>
2014-07-17 14:32:18 -0500

    04-array-we-go

---
+++ Swiftris/Array2D.swift
@@ -0,0 +1,28 @@
+//
+//  Array2D.swift
+//  Swiftris
+//
+//  Created by Stan Idesis on 7/17/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+    var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        array = Array<T?>(count:rows * columns, repeatedValue: nil)
+    }
+    
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[(row * columns) + column]
+        }
+        set(newValue) {
+            array[(row * columns) + column] = newValue
+        }
+    }
+}
